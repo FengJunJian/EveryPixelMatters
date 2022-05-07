cc1b1b9c9278bbecb8dcaf956cb0735f8e1bf019
FengJunJian <446187960@qq.com>
2022-05-07 16:33:50 +0800

    add command for featureExtractor.py

---
+++ featureExtractor.bat
@@ -0,0 +1,2 @@
+python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSSToSMDshipC DATASETS.TEST ('ship_test_SMD_cocostyle',) MODEL.WEIGHT model_0020000.pth
+python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSSToSMDshipC DATASETS.TEST ('ship_test_SeaShips_cocostyle',) MODEL.WEIGHT model_0020000.pth

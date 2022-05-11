999e0e9cf3a13eace83e41e6b4deb134da75e043
FengJunJian <446187960@qq.com>
2022-05-11 10:57:15 +0800

    command updates

--- featureExtractor.bat
+++ featureExtractor.bat
@@ -1,2 +1,5 @@
 python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSSToSMDshipC DATASETS.TEST ('ship_test_SMD_cocostyle',) MODEL.WEIGHT model_0020000.pth
 python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSSToSMDshipC DATASETS.TEST ('ship_test_SeaShips_cocostyle',) MODEL.WEIGHT model_0020000.pth
+
+python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSMDToSSshipC DATASETS.TEST ('ship_test_SMD_cocostyle',) MODEL.WEIGHT model_0020000.pth
+python tools/featureExtractor.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml OUTPUT_DIR ../logSMDToSSshipC DATASETS.TEST ('ship_test_SeaShips_cocostyle',) MODEL.WEIGHT model_0020000.pth

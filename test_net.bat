40571c5645a997c046782f961031b3da2b4b2e37
FengJunJian <446187960@qq.com>
2022-05-16 09:16:10 +0800

    updates commandline

--- test_net.bat
+++ test_net.bat
@@ -1,4 +1,8 @@
-python tools/test_net.py --config-file configs/da_ship/da_ga_ship_R_50_FPN_4x.yaml --flagVisual True --checkpoint model_0000500.pth OUTPUT_DIR ../logSSToSMDship DATASETS.TEST ('ship_test_SMD_cocostyle',)
+rem python tools/test_net.py --config-file configs/da_ship/da_ga_ca_ship_R_50_FPN_4x.yaml --flagVisual True --checkpoint model_0020000.pth OUTPUT_DIR ../logSSToSMDshipC DATASETS.TEST ('ship_test_SMD_cocostyle',)
+python tools/test_net.py --config-file configs/da_ship/da_ga_ca_ship_R_50_FPN_4x.yaml --flagVisual True --checkpoint model_0020000.pth OUTPUT_DIR ../logSMDToSSshipC DATASETS.TEST ('ship_test_SeaShips_cocostyle',)
+
+
+
 
 
 
